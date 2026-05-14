cd088aa91cfc77823d06759a4abb1268fc79761a
Andy-CE17 <andy.campos@tecsup.edu.pe>
2026-05-14 00:29:21 -0500

    Rutina Dao

--- app/src/main/java/pe/edu/tecsup/gymtrackerpro/data/local/dao/RutinaDao.kt
+++ app/src/main/java/pe/edu/tecsup/gymtrackerpro/data/local/dao/RutinaDao.kt
@@ -1,2 +1,38 @@
 package pe.edu.tecsup.gymtrackerpro.data.local.dao
 
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Update
+import androidx.room.Query
+import androidx.room.Dao
+import kotlinx.coroutines.flow.Flow
+import pe.edu.tecsup.gymtrackerpro.data.local.entity.Rutina
+
+@Dao
+interface RutinaDao {
+
+    @Insert
+    suspend fun insert(rutina: Rutina)
+
+    @Update
+    suspend fun actualizar(rutina: Rutina)
+
+    @Delete
+    suspend fun eliminar(rutina: Rutina)
+
+    @Query("SELECT * FROM Rutina WHERE usuario_id = :usuarioId ORDER BY fecha DESC")
+    fun listarRutinasPorUsuario(usuarioId: Int): Flow<List<Rutina>>
+
+    @Query("SELECT * FROM Rutina WHERE id = :rutinaId")
+    suspend fun buscarPorId(rutinaId: Int): Rutina?
+
+    @Query("SELECT * FROM Rutina WHERE usuario_id = :usuarioId")
+    suspend fun contarRutinasPorUsuario(usuarioId: Int): Int
+
+    @Query("SELECT SUM(peso_kg * repeticiones ) FROM Rutina WHERE usuario_id = :usuarioId")
+    suspend fun volumenTotal(usuarioId: Int): Double
+
+
+
+
+}
