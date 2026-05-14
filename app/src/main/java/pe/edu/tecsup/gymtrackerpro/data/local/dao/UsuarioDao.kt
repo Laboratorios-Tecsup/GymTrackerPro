886f265b0fa2334477cfbd56ec12ef928463bab9
Piero-HO <piero.huaytalla@tecsup.edu.pe>
2026-05-13 23:41:23 -0500

    Creacion de entidad Usuario

--- app/src/main/java/pe/edu/tecsup/gymtrackerpro/data/local/dao/UsuarioDao.kt
+++ app/src/main/java/pe/edu/tecsup/gymtrackerpro/data/local/dao/UsuarioDao.kt
@@ -1,2 +1,14 @@
 package pe.edu.tecsup.gymtrackerpro.data.local.dao
 
+import androidx.room.*
+
+@Entity
+data class Usuario(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @ColumnInfo(name = "nombre_usuario") val nombreUsuario: String,
+    @ColumnInfo(name = "password") val password: String,
+    @ColumnInfo(name = "nombre_completo") val nombreCompleto: String,
+    @ColumnInfo(name = "email") val email: String,
+    @ColumnInfo(name = "edad") val edad: Int,
+    @ColumnInfo(name = "fecha_registro") val fechaRegistro: String
+)
