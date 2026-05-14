919c04b87e5b3edfe04bcbf987ef1cab68bcbb08
Andy-CE17 <andy.campos@tecsup.edu.pe>
2026-05-13 23:42:20 -0500

    Entidad Rutina

--- app/src/main/java/pe/edu/tecsup/gymtrackerpro/data/local/entity/Rutina.kt
+++ app/src/main/java/pe/edu/tecsup/gymtrackerpro/data/local/entity/Rutina.kt
@@ -1,2 +1,17 @@
 package pe.edu.tecsup.gymtrackerpro.data.local.entity
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Rutina(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @ColumnInfo(name = "usuario_id") val usuarioId: Int,
+    @ColumnInfo(name = "ejercicio") val ejercicio: String,
+    @ColumnInfo(name = "grupo_muscular") val grupoMuscular: String,
+    @ColumnInfo(name = "series") val series: Int,
+    @ColumnInfo(name = "repeticiones") val repeticiones: Int,
+    @ColumnInfo(name = "peso_kg") val pesoKg: Double,
+    @ColumnInfo(name = "fecha") val fecha: String
+)
