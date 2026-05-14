a73b5ead1dd727e384745fa9c02d3ac71e64612a
Andy-CE17 <andy.campos@tecsup.edu.pe>
2026-05-14 00:13:45 -0500

    ForeignKey relacion entre dos tablas

--- app/src/main/java/pe/edu/tecsup/gymtrackerpro/data/local/entity/Rutina.kt
+++ app/src/main/java/pe/edu/tecsup/gymtrackerpro/data/local/entity/Rutina.kt
@@ -1,9 +1,20 @@
 package pe.edu.tecsup.gymtrackerpro.data.local.entity
+
  import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.ForeignKey
 
-@Entity
+@Entity(
+    tableName = "Rutina",  // ← esta línea extra
+    foreignKeys = [ForeignKey(
+        entity = Usuario::class,
+        parentColumns = ["id"],
+        childColumns = ["usuario_id"],
+        onDelete = ForeignKey.CASCADE
+    )]
+)
+
 data class Rutina(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     @ColumnInfo(name = "usuario_id") val usuarioId: Int,
